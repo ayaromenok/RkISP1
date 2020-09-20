d826f848ff8e3fdd420c76a7501559f8f1d59d3c
Andrey Yaromenok <ayaromenok@gmail.com>
2020-09-20 08:45:22 +0000

    added script to get gstreamer dev files to build RkISP1 gstreamer plugin

---
+++ bin/getGStreamer.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-plugins-base \
+gstreamer1.0-plugins-good libgstreamer-plugins-good1.0-dev \
+gstreamer1.0-plugins-bad libgstreamer-plugins-bad1.0-dev \
+gstreamer1.0-tools
