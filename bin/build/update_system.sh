06a19a1382f13866fd46118171735d014a438a96
Andrey Yaromenok <ayaromenok@gmail.com>
2020-10-27 11:28:04 +0200

    #3 gstreamer dev files to build gstrkisp plugin

--- bin/build/update_system.sh
+++ bin/build/update_system.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 #sudo apt update
 #sudo apt upgrade
-#sudo apt install v4l-utils libncurses-dev -y
+#libgstreamer-dev also include libglib2.0-dev
+sudo apt install v4l-utils libncurses-dev libgstreamer1.0-dev libdrm-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-good1.0-dev -y
 
-sudo apt-cache --names-only search ^linux-source-* | awk '{ print $1}' | grep -w 4.4 | grep rk3399
-sudo apt-cache --names-only search ^linux-source-* | awk '{ print $1}' | grep -w 4.19
-sudo apt-cache --names-only search ^linux-source-* | awk '{ print $1}' | grep -w 5.8 | grep rockchip64
+#/build/ext/rkisp/usr/lib
+#sudo apt-cache --names-only search ^linux-source-* | awk '{ print $1}' | grep -w 4.4 | grep rk3399
+#sudo apt-cache --names-only search ^linux-source-* | awk '{ print $1}' | grep -w 4.19
+#sudo apt-cache --names-only search ^linux-source-* | awk '{ print $1}' | grep -w 5.8 | grep rockchip64
 
