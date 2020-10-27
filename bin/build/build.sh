91c852d842872e3eeeee9aaadc6eb8c920e5dc60
Andrey Yaromenok <ayaromenok@gmail.com>
2020-10-27 04:33:44 +0200

    #3 update repos in 3rd dir, remove correctly resos in build dir

--- bin/build/build.sh
+++ bin/build/build.sh
@@ -6,14 +6,20 @@ mkdir ../../3rd -p
 cd ../../3rd
 echo "\tcurrent dir is: `pwd`"
 if test -d  "${RKISP1}"; then
-        echo "\t\t${RKISP1} already exist"
+        echo "\t\t${RKISP1} already exist, updating"
+	cd ${RKISP1}
+	git pull
+	cd ..
 else
         echo "\t\tcloning ${RKISP1}"
 	git clone https://github.com/ayaromenok/camera_engine_rkisp.git
 fi
 
 if test -d  "${RKISP1_EXTRA}"; then
-	echo "\t\t${RKISP1_EXTRA} already exist"
+	echo "\t\t${RKISP1_EXTRA} already exist, updating"
+	cd ${RKISP1_EXTRA}
+	git pull
+	cd ..
 else
 	echo "\t\tcloning ${RKISP1_EXTRA}"
 	git clone https://github.com/ayaromenok/gstreamer-rockchip-extra.git
@@ -23,16 +29,16 @@ mkdir ../build -p
 cd ../build
 echo "\tcurrent dir is: `pwd`"
 if test -d  "${RKISP1}"; then
-        echo "\t\t${RKISP1} exist"
-        rm -r ${RKISP1}
+        echo "\t\t${RKISP1} exist, removing"
+        rm -r -f ${RKISP1}
 fi
 
-echo "\t\tcopy ${RKISP1}"
+echo "\t\tcopy fresh ${RKISP1}"
 cp -r ../3rd/${RKISP1} ./ 
 
 if test -d  "${RKISP1_EXTRA}"; then
-        echo "\t\t${RKISP1_EXTRA} exist"
-        rm -r ${RKISP1_EXTRA}
+        echo "\t\t${RKISP1_EXTRA} exist, removing"
+        rm -r -f ${RKISP1_EXTRA}
 fi
-echo "\t\tcopy ${RKISP1_EXTRA}"
+echo "\t\tcopy fresh ${RKISP1_EXTRA}"
 cp -r ../3rd/${RKISP1_EXTRA} ./ 
