99ab22c508d52a6409a66f5d38e9d9c1ca8a042e
Andrey Yaromenok <ayaromenok@gmail.com>
2020-10-27 14:11:09 +0200

    #3 fix doubles/missings stuff

--- bin/build/build.sh
+++ bin/build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#./update_system.sh
+./update_system.sh
 
 RKISP1=camera_engine_rkisp
 RKISP1_EXTRA=gstreamer-rockchip-extra
@@ -32,18 +32,18 @@ cd ../build
 echo "\tcurrent dir is: `pwd`"
 if test -d  "${RKISP1}"; then
         echo "\t\t${RKISP1} exist, removing"
-#        rm -r -f ${RKISP1}
+        rm -r -f ${RKISP1}
 fi
 
 echo "\t\tcopy fresh ${RKISP1}"
-#cp -r ../3rd/${RKISP1} ./ 
+cp -r ../3rd/${RKISP1} ./ 
 
 if test -d  "${RKISP1_EXTRA}"; then
         echo "\t\t${RKISP1_EXTRA} exist, removing"
-#        rm -r -f ${RKISP1_EXTRA}
+        rm -r -f ${RKISP1_EXTRA}
 fi
 echo "\t\tcopy fresh ${RKISP1_EXTRA}"
-#cp -r ../3rd/${RKISP1_EXTRA} ./ 
+cp -r ../3rd/${RKISP1_EXTRA} ./ 
 
 #build
 cd ${RKISP1}
@@ -58,7 +58,7 @@ make
 
 #check results
 cd ..
-echo "\n\n"
+echo "\n\n\nbuild results:"
 if test -f  "${RKISP1}/build/lib/librkisp.so"; then
 	echo "RkISP1 driver is:\t\t OK"
 else
--- bin/build/update_system.sh
+++ bin/build/update_system.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade
 #libgstreamer-dev also include libglib2.0-dev
 sudo apt install v4l-utils libncurses-dev libgstreamer1.0-dev libdrm-dev libgstreamer-plugins-base1.0-dev \
-	libgstreamer-plugins-good1.0-dev autopoint autotools-dev libtool-bin libxrandr-dev -y
+	libgstreamer-plugins-good1.0-dev autopoint autotools-dev libtool-bin libxrandr-dev gstreamer1.0-tools -y
 
 #/build/ext/rkisp/usr/lib
 #sudo apt-cache --names-only search ^linux-source-* | awk '{ print $1}' | grep -w 4.4 | grep rk3399
